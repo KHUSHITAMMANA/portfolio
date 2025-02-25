23b9edcf66cbc5b01dcf81fbb3036d7733a7ffc6
KHUSHI <kushitamanna085@gmail.com>
2025-02-25 15:50:49 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {BrowserRouter as Router , Routes ,Route} from 'react-router-dom'
+import Home from './Compents/Home'
+import About from './Compents/About'
+import Skills from './Compents/skills'
+import Project from './Compents/project'
+import Contanct from './Compents/contanct'
+import NavbarComp from './Compents/NavbarComp'
+
+const App = () => {
+  return (
+    
+    <Router>
+      <NavbarComp/>
+      <Routes>
+        <Route path='/'element={<Home/>}></Route>
+        <Route path='/About' element={<About/>}></Route>
+        <Route path='/skills' element={<Skills/>}></Route>
+        <Route path='/projects' element={<Project/>}></Route>
+        <Route path='/contact' element={<Contanct/>}></Route>
+</Routes>
+
+</Router>
+  )
+}
+
+export default App
